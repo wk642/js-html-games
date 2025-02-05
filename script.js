de1f6b3333c97c1fd8cc468b360ea3b2a37e66cb
Winnie Kelley <w642kelley@gmail.com>
2025-02-05 14:11:58 -0600

    generate heads or tails using Math.random and displaying what is generated

--- script.js
+++ script.js
@@ -5,11 +5,21 @@ function getHorT(userChoice) {
   let tailsChoice = document.getElementById("tails-button").value;
   // console.log(`We are in! ${tailsChoice}`);
 
-  let choiceMsg = document.getElementById("message");
+  let choiceMsg = document.getElementById("choiceMessage");
   choiceMsg.innerText = `You picked ${userChoice}`;
 
+  let coinFlipResults = "";
   let coinFlipResultMsg = document.getElementById("coinFlipResult");
-  coinFlipResultMsg.innerText = `It is:  ${coinFlipResult}!!!!!!`;
 
-  
+  // start of coin flip
+  let randomNumber = Math.random();
+  console.log(`Random : ${randomNumber}`);
+
+  if (randomNumber > 0.5) {
+    coinFlipResult = "HEADS";
+    coinFlipResultMsg.innerText = `It is:  ${coinFlipResult}!!!!!!`;
+  } else {
+    coinFlipResult = "TAILS";
+    coinFlipResultMsg.innerText = `It is:  ${coinFlipResult}!!!!!!`;
+  }
 }
